3f665a27666d0d6a9b74629dec3dd5d61575fcbc
sheid <sheid@ShKh>
2025-04-13 17:08:57 -0400

    Fixed bug

--- src/objects/Visitor.java
+++ src/objects/Visitor.java
@@ -5,6 +5,8 @@ public class Visitor extends Client {
 	public Visitor(String email, String password) {
 		super(email, password);
 		this.setParkingRate(15);
+	    super.setValidated(true);
+
 	}
 	public String getValidationStatus() {
 	    super.setValidated(true);
